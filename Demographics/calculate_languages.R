a36b717e5dd75aaa7f3eb924612223b1659c1d45
Katherine Phan <kxphan@stanford.edu>
2019-07-02 10:04:03 -0700

    Load all main languages - information.

---
+++ Demographics/calculate_languages.R
@@ -0,0 +1,27 @@
+# Demographics Analyses
+
+# Run a sample script
+sample_language <- search_category_keyword(2017, "acs5", "Language Spoken")
+facts_spanish <- search_label_keyword(year = 2017, survey = "acs1", keyword = "Spanish")
+facts_filipino <- search_label_keyword(year = 2017, survey = "acs1", keyword = "Filipino")
+facts_chinese <- search_label_keyword(year=2017, survey = "acs1", keyword = "Chinese")
+facts_russian <- search_label_keyword(year=2017, survey = "acs1", keyword = "Russian")
+facts_viet <- search_label_keyword(2017, "acs1", "Vietnamese")
+facts_arabic <- search_label_keyword(2017, "acs1", "Arabic")
+
+# Write to a workbook
+demographics <- createWorkbook()
+addWorksheet(demographics, "Spanish")
+addWorksheet(demographics, "Filipino")
+addWorksheet(demographics, "Chinese")
+addWorksheet(demographics, "Russian")
+addWorksheet(demographics, "Vietnamese")
+addWorksheet(demographics, "Arabic")
+
+writeData(demographics, "Spanish", facts_spanish)
+writeData(demographics, "Filipino", facts_filipino)
+writeData(demographics, "Chinese", facts_chinese)
+writeData(demographics, "Russian", facts_russian)
+writeData(demographics, "Vietnamese", facts_viet)
+writeData(demographics, "Arabic", facts_arabic)
+saveWorkbook(demographics, file = "Demographics.xslx", overwrite = TRUE)
--- Loading Scripts/load_functions.R
+++ Loading Scripts/load_functions.R
@@ -4,77 +4,54 @@
 detach(package:plyr)
 
 # Get all populations
-getPop <- function(var){
+getPop <- function(var, year, survey){
   get_acs(geography = "county",
           variables = var,
           county    = county_bay,
           state     = census_state,
-          year      = census_yr,
-          cache_table = TRUE) %>%
+          year      = year,
+          cache_table = TRUE,
+          survey    = survey) %>%
     group_by(NAME, variable) %>%
-    summarize(total=sum(estimate))
+    summarize(total = sum(estimate))
 }
 
 # Sample of filtering by multiple counties
-getPopBay <- function(var) {
-  getPop(var) %>%
+getPopBay <- function(var, year, survey) {
+  getPop(var, year, survey) %>%
     group_by(variable) %>%
     summarise(total=sum(total))
 }
 
 # Sample of filtering by a county
-getPopSF <- function(var) {
-  getPop(var) %>%
+getPopSF <- function(var, year, survey) {
+  getPop(var, year, survey) %>%
   filter(NAME=="San Francisco County, California") #%>%
     #summarize(total=sum(total))
 }
 
 # Sample of filtering by a city/place
-getPopOak <- function(var) {
+getPopOak <- function(var, year, survey) {
   get_acs(geography = "place",
           variables = var,
           state     = census_state,
-          year      = census_yr) %>%
+          year      = year,
+          survey = survey) %>%
     filter(NAME=="Oakland city, California") %>%
     group_by(variable) %>%
     summarize(total=sum(estimate))
 }
 
-# previously was called "result", may need to rename function in other scripts.
-result <- function(var) {
-  get_acs(geography=census_geog,
-          variables = var,
-          state = census_state,
-          county = census_county,
-          geometry = FALSE,
-          year = census_yr,
-          cache=TRUE) %>%
-    group_by(NAME, GEOID) %>%
-    summarise(total = sum(estimate)) %>%
-    filter(total > 0)
-}
-
 # Search through each of the labels for a specific keyword.
 search_label_keyword <- function(year, survey, keyword) {
-  load_variables(census_yr, census_dset, cache = TRUE) %>% 
+  dataset <- load_variables(year = year, dataset = survey, cache = TRUE) %>% 
     filter(str_detect(tolower(label), tolower(keyword)))
+  merge(dataset, getPopSF(dataset$name, year, survey), by.x='name', by.y='variable')
 }
 
 # search through each of the concepts for a specific keyword
 search_category_keyword <-function(year, survey, keyword) {
-  load_variables(census_yr, census_dset, cache = TRUE) %>% 
+  dataset <- load_variables(year, survey, cache = TRUE) %>% 
     filter(str_detect(tolower(concept), tolower(keyword)))
+  merge(dataset, getPopSF(dataset$name, year, survey), by.x='name', by.y='variable')
 }
-
-# Join the results from getPop with label of variable. 
-# (For example, B02015_001 --> Estimate!!Total)
-# Currently only using San Francisco numbers.
-# TODO: Expand to all other options and functions.
-get_results_keyword <- function(dataset) {
-  merge(dataset, getPopSF(dataset$name), by.x='name', by.y='variable')
-}
-
-# Run a sample script
-sample_two <- search_category_keyword(2017, "acs5", "Language Spoken")
-sample_one <- search_label_keyword(2017, "acs5", "Spanish")
-sample_final <- get_results_keyword(sample_one)
